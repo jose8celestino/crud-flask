5ca1349af78b64803126ec7c4e9b386ccfb360bc
jose celestino <jose8.trabalho@protonmail.com>
2023-10-03 13:58:42 -0300

    restarted whole project

--- database.py
+++
@@ -1,6 +0,0 @@
-# DATABSE.PY REPRESENTA A INSTANCIA DO BANCO DE DADOS
-# É importante que o db fique num arquivo a parte para que não gere redundanccia de importação (erro de importe circular) 
-
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
